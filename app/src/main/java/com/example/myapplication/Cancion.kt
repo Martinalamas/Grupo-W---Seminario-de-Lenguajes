e2ea4f21ce26485125c46f808144686e854f3730
Abril Danahe Merched <danahemerched@gmail.com>
2025-09-07 02:29:16 -0300

    Finalizado el Adapter y el RecyclerView para la pantalla con la lista de canciones

---
+++ app/src/main/java/com/example/myapplication/Cancion.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+data class Cancion(
+    var titulo: String,
+    var top: Int,
+    var artista: String
+)
--- app/src/main/java/com/example/myapplication/Top10Activity.kt
+++ app/src/main/java/com/example/myapplication/Top10Activity.kt
@@ -4,10 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Top10Activity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
+    lateinit var rvTop10Activity: RecyclerView
+    lateinit var cancionesAdapter: CancionAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,10 +27,37 @@ class Top10Activity : AppCompatActivity() {
             insets
         }
 
-        //val botonContinuarGracias = findViewById(R.id.botonContinuarGracias)
-        //botonContinuarGracias.setOnClickListener {
-        //    val intent = Intent(this, GraciasActivity::class.java)
-        //    startActivity(intent)
+        val botonContinuar = findViewById<AppCompatButton>(R.id.botonContinuar)
+        botonContinuar.setOnClickListener {
+            val intent = Intent(this, GraciasActivity::class.java)
+            startActivity(intent)
         }
 
+        toolbar = findViewById(R.id.toolbarTop10)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Top 10"
+
+        rvTop10Activity = findViewById(R.id.rvTop10)
+        val lm = LinearLayoutManager(this)
+        rvTop10Activity.layoutManager = lm
+
+
+        cancionesAdapter = CancionAdapter(getCanciones(), this)
+        rvTop10Activity.adapter = cancionesAdapter
     }
+}
+
+private fun getCanciones(): MutableList<Cancion>{
+    var canciones: MutableList<Cancion> = ArrayList()
+    canciones.add(Cancion(titulo = "HOLA PERDIDA",1, artista = "Luck Ra & Khea"))
+    canciones.add(Cancion(titulo = "Piel",2, artista = "Tiago PZK & Ke Personaje"))
+    canciones.add(Cancion(titulo = "LUNA",3, artista = "Feid & ATL Jacob"))
+    canciones.add(Cancion(titulo = "Que me falte todo",4, artista = "Luck Ra & Abel Pintos"))
+    canciones.add(Cancion(titulo = "Tu Misterioso Alguien",5, artista = "Miranda!"))
+    canciones.add(Cancion(titulo = "7 VIDAS",6, artista = "Maria Becerra"))
+    canciones.add(Cancion(titulo = "La_Original.mp3",7, artista = "Emilia & TINI"))
+    canciones.add(Cancion(titulo = "Como Eran Las Cosas",8, artista = "Babasonicos"))
+    canciones.add(Cancion(titulo = "Baile Inolvidable",9, artista = "Bad Bunny"))
+    canciones.add(Cancion(titulo = "Amor de Vago",10, artista = "La T y La M, Malandro"))
+    return canciones
+}
