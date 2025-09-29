8e973e19057dd9dd133adf9652ba64d3fc50926f
Martina Lamas <martina.lamas06@gmail.com>
2025-09-29 08:58:32 -0300

    Funcion recordar usuario con SharedPreferences (#8)

--- app/src/main/java/com/example/myapplication/BienvenidaActivity.kt
+++ app/src/main/java/com/example/myapplication/BienvenidaActivity.kt
@@ -11,27 +11,26 @@ import androidx.core.view.WindowInsetsCompat
 
 class BienvenidaActivity : AppCompatActivity() {
 
-    //Declaracion de variables
-    lateinit var titulo : TextView
-    lateinit var boton : Button
-    lateinit var subtitulo : TextView
-    lateinit var botonAtras : Button
-
-    lateinit var botonPlaylist : Button
-
-    lateinit var botonAgregar : Button
+    // Declaración de variables
+    lateinit var titulo: TextView
+    lateinit var boton: Button
+    lateinit var subtitulo: TextView
+    lateinit var botonAtras: Button
+    lateinit var botonPlaylist: Button
+    lateinit var botonAgregar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_bienvenida)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        //Vinculacion de las variables con su ID
+        // Vinculación de las variables con su ID
         titulo = findViewById(R.id.idTitulo)
         boton = findViewById(R.id.btnTop)
         subtitulo = findViewById(R.id.idSub)
@@ -39,40 +38,38 @@ class BienvenidaActivity : AppCompatActivity() {
         botonPlaylist = findViewById(R.id.btnPlaylist)
         botonAgregar = findViewById(R.id.btnAgregar)
 
-        //Obtencion del nombre de la Activity anterior
+        // Obtención del nombre de la Activity anterior
         val nombre = intent.getStringExtra(resources.getString(R.string.nombre))
 
-        //Establecimiento del texto del titulo
+        // Establecimiento del texto del título
         titulo.text = "¡Bienvenido $nombre!"
 
-        //Al clickear en el boton, se muestra la actividad Top10
+        // Al clickear en el botón, se muestra la actividad Top10
         boton.setOnClickListener {
             val intent = Intent(this, Top10Activity::class.java)
             startActivity(intent)
             true
         }
 
-        //Al clickear en el boton de atras, se muestra la actividad Iniciar sesion
+        // Al clickear en el botón de atrás, se muestra la actividad Iniciar sesión
         botonAtras.setOnClickListener {
-           val intent = Intent(this, InicioSesion_Activity::class.java)
+            val intent = Intent(this, InicioSesion_Activity::class.java)
             startActivity(intent)
             true
         }
 
-
-        //Al clickear el boton agregar, se muestra la actividad AgregarCancion
+        // Al clickear el botón agregar, se muestra la actividad AgregarCancion
         botonAgregar.setOnClickListener {
             val intent = Intent(this, AgregarCancion::class.java)
             startActivity(intent)
             true
         }
 
-        //Al clickear el boton playlist, se muestra la actividad Playlist
+        // Al clickear el botón playlist, se muestra la actividad Playlist
         botonPlaylist.setOnClickListener {
             val intent = Intent(this, PlaylistActivity::class.java)
             startActivity(intent)
             true
         }
-
     }
 }
